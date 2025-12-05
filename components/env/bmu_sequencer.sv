1c06077519fc01270205d62c3cbbc39d285b7930
aqdarahmad <a.abusufyan@student.aaup.edu>
2025-12-05 19:11:15 +0200

    upadte sequencer code

--- components/env/bmu_sequencer.sv
+++ components/env/bmu_sequencer.sv
@@ -1,9 +1,12 @@
-class bmu_sequencer extends uvm_sequencer #(bmu_sequence_item);
+// BMU Sequencer Class
+// Responsible for generating sequence items and passing them to the driver
+class bmu_sequencer extends uvm_sequencer#(bmu_sequence_item);
 
-`uvm_component_utils(bmu_sequencer)
+  `uvm_component_utils(bmu_sequencer)
 
-function new(string name = "bmu_sequencer", uvm_component parent);
-  super.new(name, parent);
-endfunction: new
+  // Constructor
+  function new(string name = "bmu_sequencer", uvm_component parent);
+    super.new(name, parent);
+  endfunction: new
 
 endclass: bmu_sequencer
